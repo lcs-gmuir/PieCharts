c84a2c1b86dec5ba0978a5876d88bc3844ce0f35
Griffin Muir <gmuir@lcs.on.ca>
2024-02-07 20:38:19 -0500

    made a pie chart using the same data as the 1d bar chart and put it into a new file

---
+++ PieCharts/PieChart.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  PieCharts
+//
+//  Created by Griffin Muir on 2024-02-07.
+//
+
+import SwiftUI
+import Charts
+struct PieChart: View {
+    private var coffeeSales = [
+        (name: "Americano", count: 120),
+        (name: "Cappuccino", count: 234),
+        (name: "Espresso", count: 62),
+        (name: "Latte", count: 625),
+        (name: "Mocha", count: 320),
+        (name: "Affogato", count: 50)
+    ]
+    var body: some View {
+        VStack {
+            
+            VStack {
+                Chart {
+                    ForEach(coffeeSales, id: \.name) { coffee in
+
+                        SectorMark(
+                            angle: .value("Cup", coffee.count)
+                        )
+                        .foregroundStyle(by: .value("Type", coffee.name))
+                    }
+                }
+                .frame(height: 500)
+            }
+            .padding()
+            
+        }
+    }
+}
+
+#Preview {
+    PieChart()
+}
