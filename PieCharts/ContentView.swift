a7050924d62380cd030c2e987e90fb4bb026253c
Griffin Muir <gmuir@lcs.on.ca>
2024-02-07 20:25:27 -0500

    maded a basic bar chaty with the data provided

--- PieCharts/ContentView.swift
+++ PieCharts/ContentView.swift
@@ -6,16 +6,32 @@
 //
 
 import SwiftUI
-
+import Charts
 struct ContentView: View {
+    private var coffeeSales = [
+        (name: "Americano", count: 120),
+        (name: "Cappuccino", count: 234),
+        (name: "Espresso", count: 62),
+        (name: "Latte", count: 625),
+        (name: "Mocha", count: 320),
+        (name: "Affogato", count: 50)
+    ]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            VStack {
+                Chart {
+                    ForEach(coffeeSales, id: \.name) { coffee in
+                        BarMark(
+                            x: .value("Type", coffee.name),
+                            y: .value("Cup", coffee.count)
+                        )
+                        .foregroundStyle(by: .value("Type", coffee.name))
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
