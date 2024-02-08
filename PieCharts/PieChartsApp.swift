f26df027ff185102bfd527bd9fe64ebc32c9cb1d
Griffin Muir <gmuir@lcs.on.ca>
2024-02-07 20:21:59 -0500

    Initial Commit

---
+++ PieCharts/PieChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PieChartsApp.swift
+//  PieCharts
+//
+//  Created by Griffin Muir on 2024-02-07.
+//
+
+import SwiftUI
+
+@main
+struct PieChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
