a797c1e833614f9d48b4a4da30bcfaf8d7a7c44a
Griffin Muir <gmuir@lcs.on.ca>
2024-02-08 20:23:50 -0500

    tried to make intraction possible but simulator wont launch so im commiting a restarting the app

--- PieCharts/myOwnDonutChart.swift
+++ PieCharts/myOwnDonutChart.swift
@@ -16,7 +16,20 @@ struct myOwnDonutChart: View {
         (study: "calculas", avg: 89),
         (study: "math", avg: 87)
     ]
+    @State private var selectedStudy: Int?
+    @State private var selectedSector: String?
+    
+    private func findSelectedSector(value: Int) -> String? {
 
+        var accumulatedAvg = 0
+
+        let study = Grades.first { (_, avg) in
+            accumulatedAvg += avg
+            return value <= accumulatedAvg
+        }
+
+        return study?.study
+    }
     var body: some View {
         Chart {
             ForEach(Grades, id: \.study) { studys in
@@ -24,6 +37,7 @@ struct myOwnDonutChart: View {
                            innerRadius: .ratio(0.65)
                 )
                 
+                .opacity(selectedSector == nil ? 1.0 : (selectedSector == studys.name ? 1.0 : 0.5))
                 .foregroundStyle(by: .value("subject", studys.study ))
                 .annotation(position: .overlay){
                     Text("\(studys.avg)")
@@ -32,6 +46,14 @@ struct myOwnDonutChart: View {
 
             }
 
+        }
+        .chartAngleSelection(value: $selectedStudy)
+        .onChange(of: selectedStudy) { oldValue, newValue in
+            if let newValue {
+                selectedSector = findSelectedSector(value: newValue)
+            } else {
+                selectedSector = nil
+            }
         }
     }
 }
