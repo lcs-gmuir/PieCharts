061fb61c567863f3bbd5903ac835e57a932557e3
Griffin Muir <gmuir@lcs.on.ca>
2024-02-08 19:52:54 -0500

    added the ability to highlight within the chart

--- PieCharts/donutChart.swift
+++ PieCharts/donutChart.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Charts
+
 struct donutChart: View {
     private var coffeeSales = [
         (name: "Americano", count: 120),
@@ -16,20 +17,39 @@ struct donutChart: View {
         (name: "Mocha", count: 320),
         (name: "Affogato", count: 50)
     ]
+    //making the chart interactable
+    @State private var selectedCount: Int?
+    @State private var selectedSector: String?
+    private func findSelectedSector(value: Int) -> String? {
+
+        var accumulatedCount = 0
+
+        let coffee = coffeeSales.first { (_, count) in
+            accumulatedCount += count
+            return value <= accumulatedCount
+        }
+
+        return coffee?.name
+    }
     var body: some View {
         VStack {
             
             VStack {
+                
                 Chart {
                     ForEach(coffeeSales, id: \.name) { coffee in
 
                         SectorMark(
                             angle: .value("Cup", coffee.count),
+                            //making the chart a donut
                             innerRadius: .ratio(0.65),
                             angularInset: 2.0
                                
                         )
+                        .opacity(selectedSector == nil ? 1.0 : (selectedSector == coffee.name ? 1.0 : 0.5))
+                        //making the chart round
                         .cornerRadius(10.0)
+                        
                         .foregroundStyle(by: .value("Type", coffee.name))
                         .annotation(position: .overlay) {
                             Text("\(coffee.count)")
@@ -39,9 +59,19 @@ struct donutChart: View {
                     }
                 }
                 .frame(height: 500)
+                //adding a background to the chart
                 .chartBackground { proxy in
                     Text("coffee")
                     .font(.system(size: 100))}
+            }
+            //making the chart interactable
+            .chartAngleSelection(value: $selectedCount)
+            .onChange(of: selectedCount) { oldValue, newValue in
+                if let newValue {
+                    selectedSector = findSelectedSector(value: newValue)
+                } else {
+                    selectedSector = nil
+                }
             }
             .padding()
             
